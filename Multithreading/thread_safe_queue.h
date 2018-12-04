e5d605761e3f3ad93978f82b126ad35bb1d1facb
antoniopan <franciscojavieryeste@hotmail.com>
2018-12-04 11:23:19 +0800

    Remove Try Pop

--- Multithreading/thread_safe_queue.h
+++ Multithreading/thread_safe_queue.h
@@ -6,27 +6,24 @@ class ConcurrentQueue
 
 public:
     // Constructor
+    // kiMaxSize is the capacity of the queue
     ConcurrentQueue(const int kiMaxSize);
     
     // Destructor
     virtual ~ConcurrentQueue();
     
     // push one element into the queue from the back
+    // if the queue is full, wait till one element is popped out
     // please print the number of elements on the console
     void Push(const Data& data);
 
     // judge whether the queue is empty
     bool Empty() const;
 
-    // try to pop one element
-    // if the queue is empty, return false; 
-    // otherwise return true and pop the front element to the output paramenter
-    // please print the number of elements on the console
-    bool TryPop(Data& popped_value);
-
-    // wait for the queue to pop one element
-    // if the queue is empty, wait till one element is pushed into the queue; 
+    // pop one element
+    // the output argument popped_value should be assigned the element popped
+    // if the queue is empty, wait till one element is pushed into the queue;
     // if the queue is not empty, pop the front element to the output parameter
     // please print the number of elements on the console
-    void WaitAndPop(Data& popped_value);
+    void Pop(Data& popped_value);
 };
