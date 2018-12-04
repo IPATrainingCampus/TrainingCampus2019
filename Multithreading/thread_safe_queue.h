d39b8972f134c6c7d21cd26a798c16d072ae7b55
Antonio Pan <franciscojavieryeste@hotmail.com>
2018-12-04 10:52:09 +0800

    implement the template class

--- Multithreading/thread_safe_queue.h
+++ Multithreading/thread_safe_queue.h
@@ -1,3 +1,9 @@
+#include <queue>
+#include "boost/thread.hpp"
+
+namespace TrainingCampus2019
+{
+
 template<typename Data>
 class ConcurrentQueue
 {
@@ -5,22 +11,104 @@ class ConcurrentQueue
 //private:
 
 public:
+    // Constructor
+    ConcurrentQueue(const int iMaxsize)
+        : m_kiMaxSize(iMaxsize)
+    {
+
+    }
+
+    // Destructor
+    ~ConcurrentQueue()
+    {
+
+    }
+
     // push one element into the queue from the back
     // please print the number of elements on the console
-    void Push(const Data& data);
+    void Push(const Data& data)
+    {
+        boost::mutex::scoped_lock lock(m_mutex);
+
+        do 
+        {
+            if (m_queueData.size() >= m_kiMaxSize)
+            {
+                m_condition.wait(lock);
+            }
+        } while (m_queueData.size() >= m_kiMaxSize);
+
+        m_queueData.push(data);
+        std::cout << "Thread ID: " << boost::this_thread::get_id() <<
+            ". Push integer " << data << ".\n";
+        m_condition.notify_one();
+
+        return;
+    }
 
     // judge whether the queue is empty
-    bool Empty() const;
+    bool Empty() const
+    {
+        boost::mutex::scoped_lock lock(m_mutex);
+
+        return m_queueData.empty();
+    }
 
     // try to pop one element
     // if the queue is empty, return false; 
     // otherwise return true and pop the front element to the output paramenter
     // please print the number of elements on the console
-    bool TryPop(Data& popped_value);
+    bool TryPop(Data& popped_value)
+    {
+        boost::mutex::scoped_lock lock(m_mutex);
+
+        if (m_queueData.empty())
+        {
+            return false;
+        }
+
+        popped_value = std::move(m_queueData.front());
+        m_queueData.pop();
+        std::cout << "Thread ID: " << boost::this_thread::get_id() <<
+            ". Pop integer " << popped_value << ".\n";
+
+        m_condition.notify_one()
+
+        return true;
+    }
 
     // wait for the queue to pop one element
     // if the queue is empty, wait till one element is pushed into the queue; 
     // if the queue is not empty, pop the front element to the output parameter
     // please print the number of elements on the console
-    void WaitAndPop(Data& popped_value);
+    void WaitAndPop(Data& popped_value)
+    {
+        boost::mutex::scoped_lock lock(m_mutex);
+
+        while (m_queueData.empty())
+        {
+            if (m_queueData.empty())
+            {
+                m_condition.wait(lock);
+            }
+        }
+
+        popped_value = std::move(m_queueData.front());
+        //std::swap(popped_value, m_queueData.front());
+        m_queueData.pop();
+        std::cout << "Thread ID: " << boost::this_thread::get_id() <<
+            ". Pop integer " << popped_value << ".\n";
+
+        m_condition.notify_one();
+
+        return;
+    }
+
+private:
+    std::queue<Data>            m_queueData;
+    boost::mutex                m_mutex;
+    boost::condition_variable   m_condition;
+    const unsigned int          m_kiMaxSize;
 };
+
+}
