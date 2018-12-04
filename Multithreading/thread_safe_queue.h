bdd969eeaedc78e0da330429f7de65d7ee962771
antoniopan <franciscojavieryeste@hotmail.com>
2018-12-04 13:07:59 +0800

    Remove Template Implementation

--- Multithreading/thread_safe_queue.h
+++ Multithreading/thread_safe_queue.h
@@ -1,6 +1,3 @@
-#include <queue>
-#include "boost/thread.hpp"
-
 namespace TrainingCampus2019
 {
 
@@ -29,30 +26,13 @@ public:
     // please print the number of elements on the console
     void Push(const Data& data)
     {
-        boost::mutex::scoped_lock lock(m_mutex);
-
-        do 
-        {
-            if (m_queueData.size() >= m_kiMaxSize)
-            {
-                m_condition.wait(lock);
-            }
-        } while (m_queueData.size() >= m_kiMaxSize);
-
-        m_queueData.push(data);
-        std::cout << "Thread ID: " << boost::this_thread::get_id() <<
-            ". Push integer " << data << ".\n";
-        m_condition.notify_one();
-
-        return;
+        
     }
 
     // judge whether the queue is empty
     bool Empty() const
     {
-        boost::mutex::scoped_lock lock(m_mutex);
-
-        return m_queueData.empty();
+        
     }
     // pop one element out of the queue from the front
     // pop one element
@@ -62,32 +42,8 @@ public:
     // please print the number of elements on the console
     void Pop(Data& popped_value)
     {
-        boost::mutex::scoped_lock lock(m_mutex);
-
-        while (m_queueData.empty())
-        {
-            if (m_queueData.empty())
-            {
-                m_condition.wait(lock);
-            }
-        }
-
-        popped_value = std::move(m_queueData.front());
-        //std::swap(popped_value, m_queueData.front());
-        m_queueData.pop();
-        std::cout << "Thread ID: " << boost::this_thread::get_id() <<
-            ". Pop integer " << popped_value << ".\n";
-
-        m_condition.notify_one();
-
-        return;
+        
     }
-
-private:
-    std::queue<Data>            m_queueData;
-    boost::mutex                m_mutex;
-    boost::condition_variable   m_condition;
-    const unsigned int          m_kiMaxSize;
 };
 
 }
